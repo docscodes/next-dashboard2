91a301428a9ee881c5edb0932d764fbf9d2f4a08
docscodes <docscodes@gmail.com>
2025-09-04 18:32:21 -0400

    add landing page and update globals css

---
+++ app/(logged-out)/page.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/ui/button";
+import { PersonStandingIcon } from "lucide-react";
+import Link from "next/link";
+
+export default function LandingPage() {
+  return (
+    <>
+      <h1 className="flex gap-2 items-center">
+        <PersonStandingIcon
+          size={50}
+          className="text-primary"
+        />
+        My Dashboard
+      </h1>
+
+      <p>The best dashboard to manage customers</p>
+
+      <div className="flex gap-2 items-center">
+        <Button asChild>
+          <Link href="/login">Log in</Link>
+        </Button>
+
+        <small>or</small>
+
+        <Button
+          asChild
+          variant="outline"
+        >
+          <Link href="/signup">Sign up</Link>
+        </Button>
+      </div>
+    </>
+  );
+}
