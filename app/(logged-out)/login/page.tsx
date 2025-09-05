ff6f99282920ef2b712358e95261075666bf971e
docscodes <docscodes@gmail.com>
2025-09-05 18:35:36 -0400

    login page and card

---
+++ app/(logged-out)/login/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { PersonStandingIcon } from "lucide-react";
+import Link from "next/link";
+
+export default function LoginPage() {
+  return (
+    <>
+      <PersonStandingIcon size={50} />
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Login to your account</CardDescription>
+        </CardHeader>
+        <CardContent>Login Form</CardContent>
+        <CardFooter className="justify-between">
+          <small>Don&apos;t have an account?</small>
+          <Button
+            asChild
+            variant="outline"
+            size="sm"
+          >
+            <Link href="/signup">Sign up</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
+}
