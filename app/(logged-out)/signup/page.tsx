66d040de8f79ae693b013535ea17b79231ec87cd
docscodes <docscodes@gmail.com>
2025-09-06 12:06:15 -0400

    date of birth field with popover

--- app/(logged-out)/signup/page.tsx
+++ app/(logged-out)/signup/page.tsx
@@ -17,6 +17,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Popover, PopoverTrigger } from "@/components/ui/popover";
 import {
   Select,
   SelectContent,
@@ -25,7 +26,7 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { PersonStandingIcon } from "lucide-react";
+import { CalendarIcon, PersonStandingIcon } from "lucide-react";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
@@ -171,6 +172,32 @@ export default function SignupPage() {
                   />
                 </>
               )}
+
+              <FormField
+                control={form.control}
+                name="dob"
+                render={({ field }) => {
+                  return (
+                    <FormItem className="flex flex-col pt-2">
+                      <FormLabel>Date of birth</FormLabel>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <FormControl>
+                            <Button
+                              variant="outline"
+                              className="normal-case flex justify-between pr-1"
+                            >
+                              <span>Pick a date</span>
+                              <CalendarIcon />
+                            </Button>
+                          </FormControl>
+                        </PopoverTrigger>
+                      </Popover>
+                      <FormMessage />
+                    </FormItem>
+                  );
+                }}
+              />
 
               <Button type="submit">Sign up</Button>
             </form>
