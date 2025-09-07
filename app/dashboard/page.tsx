6bd3842d0c648aa4c8611f091246470b20c3a1da
docscodes <docscodes@gmail.com>
2025-09-07 09:43:41 -0400

    add tabs

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,22 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 const DashboardPage = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Tabs defaultValue="employees">
+      <TabsList>
+        <TabsTrigger value="employees">Employees stats</TabsTrigger>
+        <TabsTrigger value="teams">Teams stats</TabsTrigger>
+      </TabsList>
+
+      <TabsContent value="employees">
+        <p>Employees stats</p>
+      </TabsContent>
+
+      <TabsContent value="teams">
+        <p>Teams stats</p>
+      </TabsContent>
+    </Tabs>
+  );
 };
 
 export default DashboardPage;
