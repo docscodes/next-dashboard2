7ea0055bdab82e91a40fad98c8e497a00b77cc5a
docscodes <docscodes@gmail.com>
2025-09-07 12:48:41 -0400

    add the team leaders card content

--- app/dashboard/components/teams/teams-stats.tsx
+++ app/dashboard/components/teams/teams-stats.tsx
@@ -1,9 +1,12 @@
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import cm from "@/public/images/cm.jpg";
 import rl from "@/public/images/rl.jpg";
 import tf from "@/public/images/tf.jpg";
-import { ListChecksIcon, UsersIcon } from "lucide-react";
+import { ListChecksIcon, StarIcon, UsersIcon } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 const teamLeaders = [
@@ -75,9 +78,34 @@ export default function TeamsStats() {
           <CardHeader className="pb-2">
             <CardTitle className="text-base flex justify-between items-center">
               <span>Team leaders</span>
+              <StarIcon className="text-yellow-500" />
             </CardTitle>
           </CardHeader>
-          <CardContent className="flex flex-wrap gap-2"></CardContent>
+          <CardContent className="flex flex-wrap gap-2">
+            {teamLeaders.map((teamLeader) => (
+              <TooltipProvider key={`${teamLeader.firstName}${teamLeader.lastName}`}>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Avatar>
+                      {!!teamLeader.avatar && (
+                        <Image
+                          src={teamLeader.avatar}
+                          alt={`${teamLeader.firstName} ${teamLeader.lastName} avatar`}
+                        />
+                      )}
+                      <AvatarFallback>
+                        {teamLeader.firstName[0]}
+                        {teamLeader.lastName[0]}
+                      </AvatarFallback>
+                    </Avatar>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    {teamLeader.firstName} {teamLeader.lastName}
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            ))}
+          </CardContent>
         </Card>
         <Card>
           <CardHeader className="pb-2">
