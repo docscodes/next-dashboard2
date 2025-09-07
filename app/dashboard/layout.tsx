2125f3c1dbcc820ef2f24241b68ec09ec6062934
docscodes <docscodes@gmail.com>
2025-09-06 21:27:36 -0400

    sidebar main menu

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import MainMenu from "./components/main-menu";
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="grid grid-cols-[250px_1fr] h-screen">
-      <div className="bg-muted overflow-auto p-4">side panel</div>
+      <MainMenu />
       <div className="overflow-auto py-2 px-4">
         <h1 className="pb-4">Welcome back, Tom!</h1>
         {children}
