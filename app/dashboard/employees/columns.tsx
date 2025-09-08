6c9fff79057fa6c8dca95eccf83de7cde6efd6a0
docscodes <docscodes@gmail.com>
2025-09-08 12:41:31 -0400

    add avatar and badge to employees table

--- app/dashboard/employees/columns.tsx
+++ app/dashboard/employees/columns.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
 import { ColumnDef } from "@tanstack/react-table";
+import Image from "next/image";
 
 export type Employee = {
   id: number;
@@ -15,6 +18,28 @@ export const columns: ColumnDef<Employee>[] = [
   {
     accessorKey: "avatar",
     header: "",
+    cell: ({ row }) => {
+      const avatar: string = row.getValue("avatar");
+      const firstName: string = row.getValue("firstName");
+      const lastName: string = row.getValue("lastName");
+
+      return (
+        <Avatar>
+          {!!avatar && (
+            <Image
+              height={40}
+              width={40}
+              src={avatar}
+              alt={`${firstName} ${lastName} avatar`}
+            />
+          )}
+          <AvatarFallback className="uppercase">
+            {firstName[0]}
+            {lastName[0]}
+          </AvatarFallback>
+        </Avatar>
+      );
+    },
   },
   {
     accessorKey: "firstName",
@@ -31,5 +56,9 @@ export const columns: ColumnDef<Employee>[] = [
   {
     accessorKey: "isTeamLeader",
     header: "",
+    cell: ({ row }) => {
+      const isTeamLeader: boolean = row.getValue("isTeamLeader");
+      return isTeamLeader ? <Badge variant="success">Team leader</Badge> : null;
+    },
   },
 ];
